e72ab737b22080bb5e8530d59fb7b538ae4ccb2e
Patrycja Lamik <patrycjalamik94@gmail.com>
2023-01-09 16:57:06 +0100

    123

---
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import style from './FeedBackOptions.module.css';
+
+const INITIAL_STATE = {
+    name: '',
+    number: '',
+  };
